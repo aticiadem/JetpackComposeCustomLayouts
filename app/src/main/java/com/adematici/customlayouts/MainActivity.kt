d34d27c7f9c209be7c565dce8b5aaef3e0ab4685
AdemAtici <adematicis41@gmail.com>
2024-07-15 19:49:07 +0300

    Add custom layout and use it

--- app/src/main/java/com/adematici/customlayouts/MainActivity.kt
+++ app/src/main/java/com/adematici/customlayouts/MainActivity.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.MeasurePolicy
+import androidx.compose.ui.unit.dp
 import com.adematici.customlayouts.ui.theme.CustomLayoutsTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,53 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CustomLayoutsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyScreen() {
+    CustomGridLayout(modifier = Modifier.statusBarsPadding(), columns = 2) {
+        Text("Item 1", Modifier.padding(8.dp))
+        Text("Item 2", Modifier.padding(8.dp))
+        Text("Item 3", Modifier.padding(8.dp))
+        Text("Item 4", Modifier.padding(8.dp))
+        Text("Item 5", Modifier.padding(8.dp))
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    CustomLayoutsTheme {
-        Greeting("Android")
+private fun CustomGridLayout(
+    modifier: Modifier = Modifier,
+    columns: Int,
+    content: @Composable () -> Unit,
+) {
+    val measurePolicy = MeasurePolicy { measurables, constraints ->
+        val columnWidth = constraints.maxWidth / columns
+        val placeables = measurables.map { measurable ->
+            measurable.measure(constraints.copy(maxWidth = columnWidth))
+        }
+        val height = placeables.maxOfOrNull { it.height } ?: 0
+
+        layout(width = constraints.maxWidth, height = height * (measurables.size / columns + 1)) {
+            var xPosition = 0
+            var yPosition = 0
+            placeables.forEachIndexed { index, placeable ->
+                placeable.placeRelative(x = xPosition, y = yPosition)
+                xPosition += columnWidth
+                if ((index + 1) % columns == 0) {
+                    xPosition = 0
+                    yPosition += height
+                }
+            }
+        }
     }
+
+    Layout(
+        content = content,
+        modifier = modifier,
+        measurePolicy = measurePolicy,
+    )
 }
